af5818b6c75305a4cbe58d9ad06e6174e808fc94
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-02 16:44:26 +0600

    explore primitive types

---
+++ src/Basic-types-of-typescript/primitive.ts
@@ -0,0 +1,15 @@
+//* JS - TS
+//* string, number, boolean, undefined, null
+
+//* TS : nevel, unknown, void
+
+let userName: string = "rana123";
+
+userName = "rana222";
+
+let userId = 123;
+
+let isAdmin: boolean = false;
+isAdmin = true;
+
+let x: undefined = undefined;
