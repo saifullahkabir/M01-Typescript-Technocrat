e65d5761abf4eab2e3761fbee4e896b5276b13c8
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-02 22:31:33 +0600

    explore union and intersection

---
+++ src/Basic-types-of-typescript/unionAndIntersection.ts
@@ -0,0 +1,38 @@
+//* Union : |
+
+type UserRole = "admin" | "user";
+
+const getDashboard = (role: UserRole) => {
+  if (role === "admin") {
+    return "Admin Dashboard";
+  } else if (role === "user") {
+    return "user Dashboard";
+  } else {
+    return "Guest Dashboard";
+  }
+};
+
+// console.log(getDashboard("guest"));
+
+//* intersection : &
+
+type Employee = {
+  id: string;
+  name: string;
+  phoneNo: string;
+};
+
+type Manager = {
+  designation: string;
+  teamSize: number;
+};
+
+type EmployeeManager = Employee & Manager;
+
+const ChowdhuryShaheb: EmployeeManager = {
+  id: "123",
+  name: "Chowdhury Shaheb",
+  phoneNo: "0180000000",
+  designation: "Manager",
+  teamSize: 20,
+};
