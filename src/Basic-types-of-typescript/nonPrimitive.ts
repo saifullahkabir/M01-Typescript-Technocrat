d49b8b4d7d9ed3a799871a1f67f54c2efea337dc
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-02 17:04:53 +0600

    explore non primitive types

---
+++ src/Basic-types-of-typescript/nonPrimitive.ts
@@ -0,0 +1,16 @@
+//* array , object
+
+const bazarList: string[] = ["tomato", "eggs", "sugar", "milk"];
+
+// bazarList.push(12)
+
+let mixedArr: (string | number)[] = ["eggs", 12, "sugar", 1, "milk", 2];
+
+//* tuple
+let coordinates: [number, number] = [20, 30];
+
+let couple: [string, string] = ["Husband", "Wife"];
+
+let RanaNameAndRoll: [string, number] = ["Rana", 100];
+
+let destination: [string, string, number] = ["Chattogram", "Dhaka", 7];
