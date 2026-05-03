efa76d27df23b858138b17e96cea751aa82a3f45
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-04 02:06:40 +0600

    explore generics in typescript

---
+++ src/Advance-types-of-typescript/generics.ts
@@ -0,0 +1,33 @@
+//* generics: dynamically generalize
+
+type GenericArray<T> = Array<T>;
+
+// const friends: string[] = ["Mr. X", "Mr. Y", "Mr. Z"];
+const friends: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
+
+// const rollNumbers: number[] = [4, 7, 11];
+const rollNumbers: GenericArray<number> = [4, 7, 11];
+
+// const isEligibleList: boolean[] = [true, false, true];
+const isEligibleList: GenericArray<boolean> = [true, false, true];
+
+//* Array of object
+const userList: GenericArray<{ name: string; age: number }> = [
+  {
+    name: "Mr. X",
+    age: 22,
+  },
+  {
+    name: "Mr. Y",
+    age: 21,
+  },
+];
+
+//* tuple in generics
+type Coordinates<X, Y> = [X, Y];
+
+const coordinates1: Coordinates<number, number> = [20, 30];
+
+const coordinates2: Coordinates<string, string> = ["20", "30"];
+
+const coordinates3: Coordinates<number, string> = [20, "30"];
