13f3e69331b1f4769580d94e41cb4a4a1428ffc2
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-07 00:46:13 +0600

    type guard using typeof and in

---
+++ src/Object-Oriented-Typescript/typeGuard.ts
@@ -0,0 +1,34 @@
+
+//* typeof guard
+type Alphaneumeric = number | string;
+
+const add = (num1: Alphaneumeric, num2: Alphaneumeric): Alphaneumeric => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    return num1.toString() + num2.toString();
+  }
+};
+
+console.log(add(2, 2));
+console.log(add("2", "2"));
+
+//* in guard
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`${user.name} and his role is: ${user.role}`);
+  } else{
+    console.log(`${user.name}`);
+  }
+};
+
+getUserInfo({name: "Normal", role: "Admin"});
